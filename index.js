3fb779cec477112a021a590de50f050c2c104b1f
Faith Makinde <faithmakinde0@gmail.com>
2022-08-21 18:15:27 +0100

    work done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
 let stageEl = document.getElementById("stage");
 let pickEl = document.getElementById("pick");
+let deleted = document.getElementById("del");
 pickEl.addEventListener("click", function(){
  let randomEmoji = Math.floor( Math.random() * fighters.length );
  let randomEmojiTwo = Math.floor( Math.random() * fighters.length );
  stageEl.textContent = `${fighters[randomEmoji]} vs ${fighters[randomEmojiTwo]}`;
 });
+deleted.addEventListener("click", function(){
+    stageEl.textContent = "";
+})
